e666253f24ec6eb3a3ba64ed9da2c7d1594447f3
hiroki-ume <h.ume1219bknb@gmail.com>
2020-04-03 06:16:08 +0000

    [update]

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -6,6 +6,12 @@ class CommentsController < ApplicationController
     comment.save
     redirect_to book_path(book)
   end
+  def destroy
+    book = Book.find(params[:book_id])
+    comment = current_user.comments.find_by(params[:book_id])
+    comment.destroy
+    redirect_to book_path(book)
+  end
   private
   def comment_params
     params.require(:comment).permit(:content, :book_id, :user_id)
